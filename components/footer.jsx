b01f174f7167ea553e72d7af9791b52ae1ccf45c
Snoopstroll <87942644+Snoopstroll@users.noreply.github.com>
2021-07-25 22:03:15 +0100

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
             <Container>
             <Row className={styles.footer}>
                 <Col md={4} sm={12} className={styles.left}>
-                    <a href="mailto:mondaymandate@gmail.com"><p className="grey-text">Email: mondaymandate@gmail.com</p></a>
+                    <a href="mailto:mondaymandate@gmail.com"><p className="grey-text">Email: info@mandatemonday.com</p></a>
                     <Link href="/episodes"><a className="grey-text epLink">Episodes</a></Link>
                 </Col>
                 <Col md={4} sm={12} className={styles.middle}>
@@ -29,7 +29,7 @@ export default function Footer() {
                     </h6>
                 </Col>
                 <Col md={4} sm={12} className={styles.right}>
-                    <p className="grey-text-mobile">Email: mandatemonday@gmail.com</p>
+                    <p className="grey-text-mobile">Email: info@mandatemonday.com</p>
                     <p className="grey-text-mobile">Episodes</p>
                     <p>Follow Us On:</p>
                     <a href="https://www.facebook.com/mandatemondaypodcast"> <FacebookIcon className={styles.iconFB} fontSize="large" /></a>
